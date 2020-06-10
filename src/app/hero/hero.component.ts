afd4ca47257027a2084ba7906d7bfc8d2d81fb55
EemranLatif <eemran.latif@gmail.com>
2020-06-10 12:28:58 -0400

    Update hero.component.ts

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -7,7 +7,7 @@ import { Hero } from '../hero';
   styleUrls:  ['./hero.component.css']
 })
 export class HeroComponent {
-  // Imran changes 
+  // Imran changes edit on server ...
   @Input() hero: Hero;
   @Output() delete = new EventEmitter();
 
